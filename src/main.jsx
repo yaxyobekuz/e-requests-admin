8add6a1ff61e7697f5a598eaf6ef3c5bddfb33f2
yaxyobekuz <pubgn9642@gmail.com>
2026-02-16 12:12:04 +0500

    feat: remove unused React Query Devtools import and component

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,6 @@ import { Provider } from "react-redux";
 
 // TanStack Query
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import queryClient from "@/app/query-client";
 
 // React
@@ -35,8 +34,6 @@ createRoot(document.getElementById("root")).render(
           mobileOffset={{ top: 24 }}
         />
       </Provider>
-
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </BrowserRouter>,
 );
