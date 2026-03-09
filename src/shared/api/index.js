aa3127d55bbe2b9b3e416af8a3f15ea74577b18b
yaxyobekuz <pubgn9642@gmail.com>
2026-03-09 17:40:32 +0500

    feat: refactor API structure by removing unused API modules and adding index file for exports

---
+++ src/shared/api/index.js
@@ -0,0 +1,9 @@
+export { authAPI } from "@/features/auth/api";
+export { adminsAPI } from "@/features/admin-management/api";
+export { adminRolesAPI } from "@/features/admin-roles/api";
+export { regionsAPI } from "@/features/regions/api";
+export { requestsAPI } from "@/features/requests/api";
+export { requestTypesAPI } from "@/features/request-types/api";
+export { servicesAPI, serviceReportsAPI } from "@/features/services/api";
+export { mskAPI } from "@/features/msk/api";
+export { settingsAPI } from "@/features/settings/api";
