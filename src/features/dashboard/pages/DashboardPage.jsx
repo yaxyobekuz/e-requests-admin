3f1d02476cf13dabfc2e80a602fde0ba3e7775d6
yaxyobekuz <pubgn9642@gmail.com>
2026-03-12 01:03:26 +0500

    feat: implement DashboardPage with TopPanel and AdminStats components, and add days data for localization

--- src/features/dashboard/pages/DashboardPage.jsx
+++ src/features/dashboard/pages/DashboardPage.jsx
@@ -1,13 +1,15 @@
+// Components
+import TopPanel from "../components/TopPanel";
+import AdminStats from "../components/AdminStats";
+
 const DashboardPage = () => {
   return (
-    <div className="p-6 max-w-screen-xl mx-auto">
-      {/* Header */}
-      <div className="mb-8">
-        <h1 className="text-3xl font-bold text-gray-900">
-          Xush kelibsiz, Admin!
-        </h1>
-        <p className="text-gray-500 mt-2">Tizim boshqaruvi paneli</p>
-      </div>
+    <div className="p-6">
+      {/* Greeting */}
+      <TopPanel />
+
+      {/* Admin stats */}
+      <AdminStats />
     </div>
   );
 };
