f90c03062b57cfb4585f0518cbf989adc5328693
yaxyobekuz <pubgn9642@gmail.com>
2026-03-12 09:45:06 +0500

    feat: add animate.css dependency for enhanced animations

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 // Styles
+import "animate.css";
+import "./styles/maps.css";
 import "./styles/index.css";
 
 // Toaster
@@ -11,15 +13,15 @@ import Routes from "@/app/routes.jsx";
 import store from "@/app/store";
 import { Provider } from "react-redux";
 
-// TanStack Query
-import { QueryClientProvider } from "@tanstack/react-query";
-import queryClient from "@/app/query-client";
-
 // React
 import { createRoot } from "react-dom/client";
 
 // Router
 import { BrowserRouter } from "react-router-dom";
+
+// TanStack Query
+import queryClient from "@/app/query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
