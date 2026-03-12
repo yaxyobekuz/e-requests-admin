a2effe9081bba46539620a07dfe924822b7ad7fc
yaxyobekuz <pubgn9642@gmail.com>
2026-03-12 09:46:10 +0500

    feat: update DashboardPage to include TotalStats and StatsByMap components

--- src/features/dashboard/pages/DashboardPage.jsx
+++ src/features/dashboard/pages/DashboardPage.jsx
@@ -1,12 +1,20 @@
 // Components
 import TopPanel from "../components/TopPanel";
+import StatsByMap from "../components/StatsByMap";
 import AdminStats from "../components/AdminStats";
+import TotalStats from "../components/TotalStats";
 
 const DashboardPage = () => {
   return (
     <div className="p-6">
       {/* Greeting */}
       <TopPanel />
+
+      {/* Total stats */}
+      <TotalStats />
+
+      {/* Stats by map */}
+      <StatsByMap />
 
       {/* Admin stats */}
       <AdminStats />
